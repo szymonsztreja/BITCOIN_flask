933857aa4771ef431b6c959470acced0fb4504e3
Szymon Sztreja <s26048@pjwstk.edu.pl>
2024-03-27 14:34:11 +0100

    Updated flask app

--- flask_server.py
+++ flask_server.py
@@ -1,13 +1,15 @@
 from flask import Flask
-import requests
+import json
+import subprocess
 
 app = Flask(__name__)
 
+def get_balance():
+    return json.loads(subprocess.check_output(["bitcoin-cli", "-conf=/home/testnet/bitcoin.conf", "getbalances"]))
+
 @app.route("/")
 def hello_world():
-    return "<p>Bitcoin!</p>"
-
-
-@app.route("/wallet")
-def get_wallet():
-    return requests.get('https://developer.bitcoin.org/reference/rpc/getbalance.html').content
+    balance = get_balance()
+    confirmed = balance["mine"]["trusted"]
+    unconfirmed = balance["mine"]["untrusted_pending"]
+    return "<h2>Zbieram na czesne tb1qgj5x6kw7a6dsx87fl5ysrfrnzxz3uj0pcp9ekm</h2><p>" + str(confirmed) + " " + str(unconfirmed) + "</p>"
